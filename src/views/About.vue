fc81f2c1e9d35f7aca471f9312b76b2b2cdc395f
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-04-29 21:58:41 +0200

    Free tutorial done with Basci todo App as result

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-8">
+    <h1>About This Awsome Vutify Todo App</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat saepe
+      quo eos cum. Consectetur doloremque, delectus a aliquid voluptas nulla
+      quas dolores amet, alias provident earum voluptatem tempore sunt
+      accusamus?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat saepe
+      quo eos cum. Consectetur doloremque, delectus a aliquid voluptas nulla
+      quas dolores amet, alias provident earum voluptatem tempore sunt
+      accusamus?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat saepe
+      quo eos cum. Consectetur doloremque, delectus a aliquid voluptas nulla
+      quas dolores amet, alias provident earum voluptatem tempore sunt
+      accusamus?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat saepe
+      quo eos cum. Consectetur doloremque, delectus a aliquid voluptas nulla
+      quas dolores amet, alias provident earum voluptatem tempore sunt
+      accusamus?
+    </p>
   </div>
 </template>
