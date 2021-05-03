fe6f677284d23a522fb27878ab09a6cb67f5c13b
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-05-03 11:06:20 +0200

    List and Task component added

---
+++ src/components/Todo/TasksList.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-list class="pt-0" flat>
+    <task v-for="task in $store.state.tasks" :key="task.id" :task="task"></task>
+  </v-list>
+</template>
+
+<script>
+export default {
+  components: {
+    task: require("./Task.vue").default,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
