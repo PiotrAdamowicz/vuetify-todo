d80dae7dce35082772a3b33fd766c4cc8a0ddb9d
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-05-03 11:25:25 +0200

    #6 finished: app rearanged into child components

---
+++ src/components/Todo/NoTasks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="no-tasks">
+    <v-icon size="100" color="primary"> mdi-check </v-icon>
+    <div class="text-h5 primary--text">No tasks</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.no-tasks {
+  opacity: 0.6;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <add-task-input />
     <tasks-list v-if="$store.state.tasks.length" />
+    <no-tasks v-else />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ export default {
   components: {
     "add-task-input": require("../components/Todo/AddTaskInput.vue").default,
     "tasks-list": require("../components/Todo/TasksList.vue").default,
+    "no-tasks": require("../components/Todo/NoTasks.vue").default,
   },
 };
 </script>
